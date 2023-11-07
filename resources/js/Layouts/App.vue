29db988bd8949cfd36ea8ebf89da313ecbcaaff6
Abdelouahab Radja <abdelouahabradja@gmail.com>
2023-11-07 10:53:59 +0100

    setup both vuetify & vue-router

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -1,5 +1,9 @@
 <template>
 <h1>Welcome to App Home</h1>
+    <v-btn>
+        Button
+    </v-btn>
+    <v-icon>mdi-home</v-icon>
 </template>
 
 <script setup>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import {createApp} from "vue";
-
+import router from "@/router.js";
 import App from "@/Layouts/App.vue";
+import vuetify from "@/vuetify.js";
 
 const app = createApp(App);
-app.mount('#app')
+app.use(router);
+app.use(vuetify)
+app.mount('#app');
