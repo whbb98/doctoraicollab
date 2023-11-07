43a582025dda7f883f24d01b151d227675b9faad
Abdelouahab Radja <abdelouahabradja@gmail.com>
2023-11-07 08:04:14 +0100

    start setup view app

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Welcome to App Home</h1>
+</template>
+
+<script setup>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import {createApp} from "vue";
+
+import App from "@/Layouts/App.vue";
+
+const app = createApp(App);
+app.mount('#app')
