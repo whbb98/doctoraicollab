3ede32fbf482744fc407b244f37e814f74c871ef
AbdElOuahab Radja <33355127+whbb98@users.noreply.github.com>
2024-01-18 15:48:20 +0100

    added filter functionality to notifications (all, unread) (#28)

--- resources/js/pages/Notifications.vue
+++ resources/js/pages/Notifications.vue
@@ -9,43 +9,84 @@
                 color="primary"
                 variant="elevated"
                 v-model="filterBy"
+                mandatory
                 on-update:model-value="">
                 <v-chip value="all"
                         :variant="filterBy === 'all' ? 'elevated' : 'outlined'">
                     all
                 </v-chip>
                 <v-chip value="unread"
-                        :variant="filterBy === 'unread' ? 'elevated' : 'outlined'">
+                        :variant="filterBy === 'unread' ? 'elevated' : 'outlined'"
+                        :disabled="unreadCount <= 0"
+                >
                     unread
                     <v-badge
-                        v-if="true"
+                        v-if="unreadCount > 0"
                         color="dark"
-                        content="12"
+                        :content="unreadCount"
                         inline
                     ></v-badge>
                 </v-chip>
             </v-chip-group>
             <v-list>
-                <v-list-item>
-                    <notification-card/>
-                </v-list-item>
-                <v-list-item>
-                    <notification-card/>
-                </v-list-item>
-                <v-list-item>
-                    <notification-card/>
-                </v-list-item>
+                <template v-for="item in dummyData">
+                    <v-list-item :key="item.id"
+                                 v-if="filterBy=='all' || (filterBy=='unread' && !item?.isRead)"
+                    >
+                        <notification-card
+                            :notification="item"
+                            @markAsRead="markAsReadHandler"/>
+                    </v-list-item>
+                </template>
             </v-list>
         </v-card-text>
     </v-card>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import NotificationCard from "@/components/NotificationCard.vue";
 
 const filterBy = ref('all')
+const unreadCount = ref(0)
+const dummyData = reactive([
+    {
+        id: '1',
+        sender: 'John Doe',
+        message: 'praesent impetus vestibulum porro quaestio no appareat pharetra.',
+        isRead: false,
+        createdAt: '16 Jan 2024 at 14:32'
+    },
+    {
+        id: '2',
+        sender: 'Amine Smahi',
+        message: 'praesent impetus vestibulum porro quaestio no appareat pharetra.',
+        isRead: true,
+        createdAt: '16 Jan 2024 at 14:32'
+    },
+    {
+        id: '3',
+        sender: 'Radja Abdelouahab',
+        message: 'praesent impetus vestibulum porro quaestio no appareat pharetra.',
+        isRead: false,
+        createdAt: '16 Jan 2024 at 14:32'
+    },
+    {
+        id: '4',
+        sender: 'Nafissa Rahali',
+        message: 'praesent impetus vestibulum porro quaestio no appareat pharetra.',
+        isRead: true,
+        createdAt: '16 Jan 2024 at 14:32'
+    }
+])
 
+unreadCount.value = dummyData.filter(item => item.isRead === false).length
+
+function markAsReadHandler(notificationID) {
+    const notification = dummyData.find(item => item.id === notificationID)
+    notification.isRead = true
+    unreadCount.value--
+}
 </script>
 
 <style scoped>
