89cd8f7bd6e8f3cbb55e271d7d2da1ef3da0f1ac
Abdelouahab Radja <abdelouahabradja@gmail.com>
2024-01-19 12:11:40 +0100

    done with card ui 'blog card'

---
+++ resources/js/components/BlogCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-card color="secondary">
+        <v-card-title class="pa-0">
+            <v-img height="200" cover src="https://picsum.photos/600/200">
+                <template #placeholder>
+                    <div class="d-flex align-center justify-center fill-height">
+                        <v-progress-circular
+                            color="primary"
+                            indeterminate
+                        ></v-progress-circular>
+                    </div>
+                </template>
+            </v-img>
+        </v-card-title>
+        <v-card-text class="text-capitalize mt-3 py-0">
+            <h3 class="mb-2">blog title</h3>
+            <p class="mb-3">
+                {{ truncatedDescription }}
+            </p>
+            <span class="d-flex align-center">
+                <v-icon>mdi-calendar</v-icon>
+                {{ new Date().toDateString() }} 14:31
+            </span>
+        </v-card-text>
+        <v-card-actions>
+            <span class="d-flex align-center mr-5">
+                <v-icon>mdi-account-group</v-icon>
+                12
+            </span>
+            <span class="d-flex align-center">
+                <v-icon>mdi-comment-multiple-outline</v-icon>
+                15
+            </span>
+            <v-spacer/>
+            <router-link to="/blogs/id" class="text-capitalize text-decoration-none text-primary">read more
+            </router-link>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup>
+
+import {computed, ref} from "vue";
+
+const description = ref(
+    'Atomorummetus iaculis dictas. Deterruissetquaerendum quas instructior placerat perpetua error.\n' +
+    '                Dictumaccusata massa salutatus doming. Aptentvis vidisse putent nonumy noluisse ridiculus cras inimicus\n' +
+    '                pellentesque pharetra alienum voluptatum legere sadipscing. Sapienmovet corrumpit periculis vocent\n' +
+    '                eripuit delectus adversarium liber quaeque varius dui aliquid nominavi suscipiantur potenti civibus dui\n' +
+    '                sonet.'
+)
+const maxLength = 100
+const truncatedDescription = computed(() => {
+    if (description.value.length <= maxLength) {
+        return description.value
+    } else {
+        return description.value.slice(0, maxLength) + '...'
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/pages/Blogs.vue
+++ resources/js/pages/Blogs.vue
@@ -1,9 +1,77 @@
 <template>
-    <h1>Blogs Page</h1>
+    <v-card>
+        <v-card-title class="font-weight-bold text-capitalize">blogs</v-card-title>
+        <v-card-text class="d-flex align-center">
+            <v-chip-group
+                class="text-capitalize"
+                color="primary"
+                variant="elevated"
+                mandatory
+                v-model="filterBy"
+            >
+                <v-chip value="all" :variant="filterBy=='all'? 'elevated':'outlined'">
+                    all
+                    <v-badge
+                        color="dark"
+                        :content="blogsCount.all"
+                        inline
+                    ></v-badge>
+                </v-chip>
+                <v-chip value="myBlogs" :variant="filterBy=='myBlogs'? 'elevated':'outlined'">
+                    my blogs
+                    <v-badge
+                        color="dark"
+                        :content="blogsCount.myBlogs"
+                        inline
+                    ></v-badge>
+                </v-chip>
+                <v-chip value="participating" :variant="filterBy=='participating'? 'elevated':'outlined'">
+                    participating
+                    <v-badge
+                        color="dark"
+                        :content="blogsCount.participating"
+                        inline
+                    ></v-badge>
+                </v-chip>
+                <v-chip value="pending" :variant="filterBy=='pending'? 'elevated':'outlined'">
+                    pending
+                    <v-badge
+                        color="dark"
+                        :content="blogsCount.pending"
+                        inline
+                    ></v-badge>
+                </v-chip>
+            </v-chip-group>
+            <v-spacer/>
+            <v-btn rounded color="primary">
+                new blog
+            </v-btn>
+        </v-card-text>
+        <v-card-text>
+            <v-row>
+                <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="i in 5">
+                    <blog-card/>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script setup>
 
+import {reactive, ref, watch} from "vue";
+import BlogCard from "@/components/BlogCard.vue";
+
+const filterBy = ref('all')
+const blogsCount = reactive({
+    all: 12,
+    myBlogs: 2,
+    participating: 6,
+    pending: 3
+})
+watch(filterBy, (newValue) => {
+    console.log(newValue)
+})
 </script>
 
 <style scoped>
