cded2a9776b554034bb2f1961d080bf46d3d8f76
Abdelouahab Radja <abdelouahabradja@gmail.com>
2024-01-27 21:18:10 +0100

    formatted code

--- resources/js/components/BlogDetails.vue
+++ resources/js/components/BlogDetails.vue
@@ -1,13 +1,14 @@
 <template>
     <v-card>
         <v-card-title class="text-capitalize font-weight-bold">
-            {{route.params.blogID}}
+            {{ route.params.blogID }}
         </v-card-title>
     </v-card>
 </template>
 
 <script setup>
 import {useRoute, useRouter} from "vue-router";
+
 const route = useRoute()
 </script>
 
