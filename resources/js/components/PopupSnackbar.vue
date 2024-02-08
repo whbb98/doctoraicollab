6f700b955ae02b6a27f0e0427196d39d34a55019
Abdelouahab Radja <abdelouahabradja@gmail.com>
2024-02-08 07:32:12 +0100

    added custom pop up snackbar to display notifications

---
+++ resources/js/components/PopupSnackbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-snackbar v-model="isPopupOpened" :color="popupColor">
+        <div class="text-capitalize font-weight-bold text-subtitle-1 pb-2">
+            {{popupTitle}}
+        </div>
+        <p v-if="popupMessage" class="text-capitalize">{{popupMessage}}</p>
+        <template #actions>
+            <v-btn @click="isPopupOpened = false">
+                <v-icon>mdi-close-circle</v-icon>
+            </v-btn>
+        </template>
+    </v-snackbar>
+</template>
+
+<script setup>
+import {ref, watch} from "vue";
+
+const props = defineProps(['data'])
+const isPopupOpened = ref(null)
+const popupColor = ref(null)
+const popupTitle = ref(null)
+const popupMessage = ref(null)
+const colors = {
+    success: 'primary',
+    error: 'error',
+    info: 'secondary',
+    warning: 'warning'
+}
+watch(() => props.data, (newData) => {
+    isPopupOpened.value = newData.open
+    popupTitle.value = newData.title
+    popupMessage.value = newData.message
+    popupColor.value = colors[newData.type]
+})
+
+</script>
+
+<style scoped>
+
+</style>
