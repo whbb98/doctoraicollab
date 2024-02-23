ea4d38e5f68f6d5c35d7c00314bb22565bd29c85
AbdElOuahab Radja <33355127+whbb98@users.noreply.github.com>
2024-02-23 10:29:44 +0100

    Full stack (#45)
    * fixed comment dummy data for blog
    
    * make save btn show in edit mode
    
    * migrated AnnoViewer component from options API --> composition API

--- resources/js/components/AnnoViewer.vue
+++ resources/js/components/AnnoViewer.vue
@@ -1,65 +1,64 @@
 <template>
-    <div :id="viewerId"
+    <div :id="viewerId" ref="annoViewer"
          class="openseadragon-viewer rounded border w-100 h-100"
          @mousemove="handleMouseMove" @keydown.shift="handleShiftKey" @keyup.shift="resetCursor"
     />
 </template>
 
-<script>
+<script setup>
 import OpenSeadragon from 'openseadragon';
 import * as Annotorious from '@recogito/annotorious-openseadragon';
 import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
+import {onMounted, ref, watch} from "vue";
 
-export default {
-    props: {
-        viewerId: String,
-        imgUrl: String,
-        drawingTool: String,
-        isAnnoVisible: Boolean
-    },
-    data() {
-        return {
-            anno: null,
-        };
-    },
-    mounted() {
-        const viewer = OpenSeadragon({
-            id: this.viewerId,
-            prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@4.1/build/openseadragon/images/',
-            tileSources: {
-                type: "image",
-                url: this.imgUrl
-            }
-        });
-        const config = {}; // Optional plugin config options
-        this.anno = Annotorious(viewer, config);
-        this.anno.setDrawingTool(this.drawingTool)
-        this.anno.setVisible(this.isAnnoVisible)
-    },
-    watch: {
-        drawingTool(newVal) {
-            this.anno.setDrawingTool(newVal)
-        },
-        isAnnoVisible(newVal) {
-            this.anno.setVisible(newVal)
+const annoViewer = ref(null)
+let anno = null
+
+const props = defineProps({
+    viewerId: String,
+    imgUrl: String,
+    drawingTool: String,
+    isAnnoVisible: Boolean
+})
+
+onMounted(() => {
+    const viewer = OpenSeadragon({
+        id: props.viewerId,
+        prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@4.1/build/openseadragon/images/',
+        tileSources: {
+            type: "image",
+            url: props.imgUrl
         }
-    },
-    methods: {
-        handleMouseMove(event) {
-            if (event.shiftKey) {
-                this.$el.style.cursor = 'crosshair';
-            } else {
-                this.$el.style.cursor = 'grab';
-            }
-        },
-        handleShiftKey() {
-            this.$el.style.cursor = 'crosshair';
-        },
-        resetCursor() {
-            this.$el.style.cursor = 'grab';
-        },
+    });
+    const config = {}; // Optional plugin config options
+    anno = Annotorious(viewer, config);
+    anno.setDrawingTool(props.drawingTool)
+    anno.setVisible(props.isAnnoVisible)
+})
+
+watch(() => props.drawingTool, (newVal) => {
+    anno.setDrawingTool(newVal)
+})
+watch(() => props.isAnnoVisible, (newVal) => {
+    anno.setVisible(newVal)
+})
+
+function handleMouseMove(event) {
+    if (event.shiftKey) {
+        annoViewer.value.style.cursor = 'crosshair';
+    } else {
+        annoViewer.value.style.cursor = 'grab';
     }
-};
+}
+
+function handleShiftKey() {
+    annoViewer.value.style.cursor = 'crosshair';
+}
+
+function resetCursor() {
+    annoViewer.value.style.cursor = 'grab';
+}
+
 </script>
 
 <style scoped>
